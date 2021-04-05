16461caf87719690fe9ab921b0c3385d0ad8138e
Sajon Oso <sajonoso@users.noreply.github.com>
2021-04-05 12:38:18 +1000

    change package name

--- src/com/nil3/basixkeyboard/LatinKeyboard.java
+++ src/com/nil3/basixkeyboard/LatinKeyboard.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.example.android.softkeyboard;
+package com.nil3.basixkeyboard;
 
 import android.content.Context;
 import android.content.res.Resources;
